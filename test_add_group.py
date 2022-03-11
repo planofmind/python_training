9c1d1c80c295c1a9725381922f6b1cefc11faf17
planofmind <planofmind@github.com>
2022-03-11 12:42:20 +0300

    Сделан первый тест по добавлению группы. Убраны лишние методы и импорты.

--- test_add_group.py
+++ test_add_group.py
@@ -1,8 +1,7 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
 
 
 class TestAddGroup(unittest.TestCase):
@@ -31,20 +30,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
 
     def tearDown(self):
         self.wd.quit()
